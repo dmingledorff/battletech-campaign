8ab12652f1538a39a4db93cb82b19202ce56f6b6
dmingledorff <danielmingledorff@gmail.com>
2025-09-29 21:15:56 -0400

    added more ranks

--- SQL/ranks.sql
+++ SQL/ranks.sql
@@ -1,12 +1,17 @@
 INSERT INTO ranks (faction, full_name, abbreviation, grade) VALUES
-('Davion', 'Private',    'Pvt', 1),
-('Davion', 'Corporal',   'Cpl', 2),
-('Davion', 'Sergeant',   'Sgt', 3),
-('Davion', 'Lieutenant', 'Lt',  4),
-('Davion', 'Captain',    'Cpt', 5),
-('Davion', 'Major',      'Maj', 6),
-('Davion', 'Colonel',    'Col', 7),
-('Davion', 'Marshal',    'Msh', 8);
+('Davion', 'Private',           'Pvt.', 1),
+('Davion', 'Corporal',          'Cpl.', 2),
+('Davion', 'Sergeant',          'Sgt.', 3),
+('Davion', 'Leftenant',         'Lt.',  4),
+('Davion', 'Captain',           'Cpt.', 5),
+('Davion', 'Major',             'Maj.', 6),
+('Davion', 'Leftenant Colonel', 'Lt. Col', 7),
+('Davion', 'Colonel',           'Col.', 8),
+('Davion', 'Leftenant General', 'Lt. Gen.', 9),
+('Davion', 'Major General',     'Maj. Gen.', 10),
+('Davion', 'General',           'Gen.', 11),
+('Davion', 'Marshal',           'Msh.', 12),
+('Davion', 'Field Marshal',     'FN', 13);
 
 INSERT INTO ranks (faction, full_name, abbreviation, grade) VALUES
 ('Kurita', 'Meiyo-hei',   'Mh',  1),  -- equivalent to Private
