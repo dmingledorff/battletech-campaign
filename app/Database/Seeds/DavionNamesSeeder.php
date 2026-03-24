7059633d5d64a9c218146405a3c33c594d634b5e
dmingledorff <danielmingledorff@gmail.com>
2026-03-23 22:14:22 -0400

    Fixed table name

--- app/Database/Seeds/DavionNamesSeeder.php
+++ app/Database/Seeds/DavionNamesSeeder.php
@@ -9,7 +9,7 @@ class DavionNamesSeeder extends Seeder
     public function run()
     {
         $this->db->disableForeignKeyChecks();
-        $this->db->table('davion_names')->truncate();
+        $this->db->table('name_pool')->where('faction', 'Davion')->delete();
         $this->db->enableForeignKeyChecks();
 
         $sql = file_get_contents(APPPATH . '../SQL/davion_names.sql');
